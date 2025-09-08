64c5d130fd25d952fd60b1ecdd7a8cf20b3f3fc5
brentamage <brentgalpez@gmail.com>
2025-09-08 08:25:58 +0800

    change api from local to render

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/projects';
-
+// const API_URL = 'http://localhost:5000/api/projects';
+const API_URL = 'https://lifewood-backend.onrender.com/api/projects';
 export const getAllProjects = async () => {
   const response = await axios.get(API_URL);
   return response.data;
