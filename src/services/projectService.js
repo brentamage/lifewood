cd649b0c9149f720bd1613e0f5b35d2e21c711db
brentamage <brentgalpez@gmail.com>
2025-09-08 08:37:50 +0800

    change api source

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 
 // const API_URL = 'http://localhost:5000/api/projects';
 const API_URL = 'https://lifewood-backend.onrender.com/api/projects';
-const API_URL = `${import.meta.env.VITE_API_URL}/api/projects`;
 
 /**
  * Fetch all projects from the backend.
